8c09b065b985f558d9b1bfebd99fc6b0d5e9c20a
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-03-05 22:40:38 +0100

    Dotfiles megaupdate :3

--- install.py
+++ install.py
@@ -2,23 +2,24 @@ import os
 import shutil
 from pathlib import Path
 
-pacman_pkgs = "alsa-utils exa feh mpv picom pulseaudio python ranger vim xfce4 Xorg xdo"
-yay_pkgs = "pasfetch pasmotd g810-led-git"
-dotfiles = [".bashrc", ".xinitrc"]
+pacman_pkgs = "alsa-utils exa nitrogen flameshot mpv picom pulseaudio python ranger vim xorg xdo fpc
+polybar arandr"
+paru_pkgs = "pasfetch pasmotd g810-led-git leftwm alacritty"
+dotfiles = [".bashrc", ".xinitrc", "start.sh"]
 dotdirs = [".config"]
 
-def install_yay():
-    print (":: Installing yay...")
-    os.system("git clone https://aur.archlinux.org/yay")
-    os.chdir("yay")
+def install_paru():
+    print (":: Installing paru...")
+    os.system("git clone https://aur.archlinux.org/paru")
+    os.chdir("paru")
     os.system("makepkg -s")
     for file in os.listdir():
         if file.endswith(".zst"):
             os.system("sudo pacman -U "+file)
 
-    print (":: yay installed successfully, cleaning")
+    print (":: paru installed successfully, cleaning")
     os.chdir("..")
-    os.system("rm -rf yay")
+    os.system("rm -rf paru")
 
 def filecount():
     count = 0
@@ -29,15 +30,15 @@ def filecount():
     return count
 
 def main():
-    print (":: Checking if yay is installed")
-    if not os.path.isfile("/usr/bin/yay"):
-        install_yay()
+    print (":: Checking if paru is installed")
+    if not os.path.isfile("/usr/bin/paru"):
+        install_paru()
 
     print ("\n:: Installing pacman pkgs")
     os.system("sudo pacman -S "+pacman_pkgs)
 
-    print ("\n:: Installing yay pkgs")
-    os.system("yay -a -S "+yay_pkgs)
+    print ("\n:: Installing paru pkgs")
+    os.system("paru -S "+paru_pkgs)
 
     print ("\n:: Copying dotfiles...")
     total = len(dotfiles)+filecount()
@@ -64,6 +65,9 @@ def main():
                 copied += 1
 
     print (":: Copied all files!")
+
+    if not input(":: Do you wish to install nerd-fonts-complete-starhsip? [Y/n] ") == "n":
+        os.system("paru -S nerd-fonts-starship-complete")
     print (":: Install successfull!")
 
 if __name__ == "__main__":
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+polybar top_lg &
+polybar top_acer &
+nitrogen --restore &
+sh .screenlayout/layout.sh
+picom &
